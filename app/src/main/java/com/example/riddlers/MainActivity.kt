8f5cf38636a1726c5be7da010a7a4c66a6f696e1
Gabriel Jose S. Esperanza <esperanzagabrieljose@gmail.com>
2024-07-26 21:20:45 +0800

    removing the quit button(not necessary) also added some image assets(icon update)

--- app/src/main/java/com/example/riddlers/MainActivity.kt
+++ app/src/main/java/com/example/riddlers/MainActivity.kt
@@ -1,16 +1,34 @@
 package com.example.riddlers
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.ImageButton
+import androidx.core.view.setPadding
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // playButton going to gameplay (star)
+        val playButton: ImageButton = findViewById(R.id.btnPlay)
+        playButton.setOnClickListener{
+            val intent = Intent(this,Gameplay::class.java)
+            startActivity(intent)
+        }
+
+
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
